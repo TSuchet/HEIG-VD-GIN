3e8d76e4bc57201705b4df355899c113a41d462b
Corentin Junod <corentin.junod@outlook.com>
2025-04-14 08:55:52 +0200

    Fix exercice 09

--- exercices/solutions/exercice_09/ex09/main.js
+++ exercices/solutions/exercice_09/ex09/main.js
@@ -6,43 +6,51 @@ import TileLayer from 'ol/layer/Tile.js';
 import WMTS, {optionsFromCapabilities} from 'ol/source/WMTS.js';
 
 const parser = new WMTSCapabilities();
-let map;
 
 fetch('https://wmts.geo.admin.ch/EPSG/3857/1.0.0/WMTSCapabilities.xml?lang=fr')
-  .then(function (response) {
-    return response.text();
-  })
-  .then(function (text) {
-    const result = parser.read(text);
-    const options_1 = optionsFromCapabilities(result, {
-      layer: 'ch.swisstopo.swisssurface3d-reliefschattierung-multidirektional',
-      matrixSet: 'EPSG:3857',
-    });
-
-    const options_2 = optionsFromCapabilities(result, {
-      layer: 'ch.swisstopo.swisstlm3d-strassen',
-      matrixSet: 'EPSG:3857',
-    });
-
-    map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          opacity: 1,
-          source: new WMTS(options_1),
-        }),
-        new TileLayer({
-          opacity: 1,
-          source: new WMTS(options_2),
-        }),
-      ],
-      view: new View({
-        center: [854900, 5894900],
-        zoom: 8,
-      }),
-    });
+.then(function (response) {
+  return response.text();
+})
+.then(function (text) {
+  const result = parser.read(text);
+  const options_1 = optionsFromCapabilities(result, {
+    layer: 'ch.swisstopo.swisssurface3d-reliefschattierung-multidirektional',
+    matrixSet: 'EPSG:3857',
   });
 
-const opacityInput = document.getElementById('opacity-input');
-opacityInput.addEventListener('input', () => osm.setOpacity( parseFloat(opacityInput.value)));
-update();
+  const options_2 = optionsFromCapabilities(result, {
+    layer: 'ch.swisstopo.swisstlm3d-strassen',
+    matrixSet: 'EPSG:3857',
+  });
+
+  const road_tilelayer = new TileLayer({
+    opacity: 1,
+    source: new WMTS(options_2),
+  })
+
+  new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        opacity: 1,
+        source: new WMTS(options_1),
+      }),
+      road_tilelayer
+    ],
+    view: new View({
+      center: [854900, 5894900],
+      zoom: 8,
+    }),
+  });
+
+
+  const opacityInput = document.getElementById('opacity-input');
+
+  function update(){
+    road_tilelayer.setOpacity( parseFloat(opacityInput.value))
+  }
+  
+  opacityInput.addEventListener('input', update);
+  update()
+
+});
